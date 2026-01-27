f593b8f03fc28ab22c323a85be2b872aff1009be
link10804k <62154757+link10804k@users.noreply.github.com>
2026-01-27 15:04:51 +0100

    Ejercicio 5

--- IGProjects_x64_VS2022/IG1App/Scene.cpp
+++ IGProjects_x64_VS2022/IG1App/Scene.cpp
@@ -19,7 +19,8 @@ Scene::init()
 	// Graphics objects (entities) of the scene
 	gObjects.push_back(new RGBAxes(400.0));
 
-	gObjects.push_back(new RegularPolygon(7, 100, { 1, 0, 0, 1 }));
+	gObjects.push_back(new RegularPolygon(6, 100, { 1, 0, 1, 1 }));
+	gObjects.push_back(new RegularPolygon(60, 100, {1, 1, 0, 1}));
 }
 
 Scene::~Scene()
