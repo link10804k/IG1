d166011ddf394b0a551ed6a6c53b389dedaac701
Javier Zazo Morillo <javiezaz@ucm.es>
2026-01-28 11:02:21 +0100

    Ejercicio 9

--- IGProjects_x64_VS2022/IG1App/Entity.cpp
+++ IGProjects_x64_VS2022/IG1App/Entity.cpp
@@ -46,15 +46,16 @@ EntityWithColors::render(mat4 const& modelViewMat) const
 		for (int i = 0; i < 3; ++i) {
 			zAxis[i] = modelViewMat[2][i];
 			zModel[i] = mModelMat[2][i];
-		}
+		}		
 
 		if (glm::dot(zAxis, zModel) < 0) {
-			mShaderAlt->use();
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 		}
 		else {
-			mShader->use();
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		}
-		
+
+		mShader->use();
 		upload(aMat);
 		mMesh->render();
 	}
