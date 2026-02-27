0bae4b0637c005661fbd80733fdd9f27d8e9d9b3
frampth <frampth@proton.me>
2026-02-27 15:27:38 +0100

    cmake

--- IGProjects_x64_VS2022/build/cmake_install.cmake
+++ IGProjects_x64_VS2022/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/frampth/Documents/Clase/Curso2/Cuatri2/IG/IG1/IGProjects_x64_VS2022
+# Install script for directory: /home/frampth/Documents/Clase/IG1/IGProjects_x64_VS2022
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,7 +45,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/frampth/Documents/Clase/Curso2/Cuatri2/IG/IG1/IGProjects_x64_VS2022/build/install_local_manifest.txt"
+  file(WRITE "/home/frampth/Documents/Clase/IG1/IGProjects_x64_VS2022/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/frampth/Documents/Clase/Curso2/Cuatri2/IG/IG1/IGProjects_x64_VS2022/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/frampth/Documents/Clase/IG1/IGProjects_x64_VS2022/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
